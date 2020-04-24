2153f39a009d0ccbed6b3c4f72f574a569b75697
Alex Mason <Axman6@gmail.com>
2020-04-25 02:59:29 +1000

    Update Main and cabal

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,15 @@
 module Main (main) where
 
-import Havenza
+import Data.IORef (newIORef)
 
 import Servant.Server
 import Network.Wai.Handler.Warp (run)
 
+import Havenza
+
+
 main :: IO ()
-main = run 8080 (serve webApi avenzaHandlers)
+main = do
+  ref <- newIORef emptyAppState
+  run 8080 $ serve webApi $ 
+    hoistServer webApi (runApp ref) avenzaHandlers
